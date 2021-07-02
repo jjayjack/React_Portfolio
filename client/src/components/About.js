2c7ead478f2e0ad2992533286376b4c707ef4f5b
jjayjack <jocelynjayjack@gmail.com>
2021-07-02 14:20:19 -0500

    Updated about me

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -8,19 +8,14 @@ const About = () => {
             </div>
             <div className="content">
                 <div id="portrait" alt="Portrait of Jocelyn"/>
-                <p className="text-content">Hello there! My name is Jocelyn and this is my site that showcases
-                    projects and activities that I have done while coding. I am excited
-                    to start learning new languages and diving deep within the code.
-                    I am originally from Northwest Indiana where I spent time with my loving
-                    parents and sister. After high school, I went to Purdue University where
-                    I studied Nutrition, Fitness and Health and graduated with my Bachelor Degree.
-                    Once I graduated, I was undecided what I wanted to do with my degree. I decided
-                    to take some time off from school and took a position with Lowe's to help
-                    me work on paying off my student loans. Once I finished paying off my loan I
-                    decided that it was time to figure out what I wanted to do next. I had moved to
-                    Chicago with my boyfriend and had the time to think about what I wanted to do. This
-                    led me to evaluate what I enjoy doing and pointed me toward pursuing Trilogy's Bootcamp
-                    through Northwestern.</p>
+                <p className="text-content">Hello there! I'm Jocelyn and this is my site that showcases
+                    projects and activities that I have done while coding. I am a Full-stack web developer with a background 
+                    in Nutrition and Kinesiology looking to navigate into a new role as 
+                    a Front-end software engineer to create optimized user interface. 
+                    From going from no-experience to earning a certificate in Full-Stack Web Development 
+                    from Northwestern University Coding Boot Camp, I have found a driving interest in Javascript, 
+                    React, Nodejs, and Axios. I am an organized, detail-orientated developer who enjoys learning 
+                    new concepts and seeing a project complete while working with a team to better accomplish goals. </p>
             </div>    
         </div>
     )
